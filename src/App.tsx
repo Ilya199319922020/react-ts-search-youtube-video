4474143beb0d7700b64a71392c34841dd0acb43b
Ilya Smirnov <iliasmirnov93@mail.ru>
2022-10-31 01:26:43 +0300

    fixed bugs publich github pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Header from './components/Header/Header';
 function App() {
   return (<>
     <Routes>
-      <Route path='/' element={<Navigate replace to="/login" />} />
+      <Route path='/react-ts-search-youtube-video' element={<Navigate replace to="/login" />} />
       <Route path='/login' element={<AuthPage />} />
       <Route path='/' element={<Header />}>
         <Route path='search/*' element={<Container />} />
